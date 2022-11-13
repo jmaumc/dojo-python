087be91a82483a0c72300e3f2691d912b4f08c75
Mauricio Mejia Castro <mauricio.mejia@promptlyhealth.com>
2022-11-13 22:38:37 +0000

    Add dataframe exploration script

--- rp-lp-pandas/01-exploring-dataset.py
+++ rp-lp-pandas/01-exploring-dataset.py
@@ -8,9 +8,19 @@ download_url = "https://raw.githubusercontent.com/fivethirtyeight/data/master/nb
 
 nba = pd.read_csv(download_url)
 
-print(f"Shape: {nba.shape}")
-print(f"N. rows: {len(nba)}")
-#print(nba.head(10))
+#print(f"Shape: {nba.shape}")
+#print(f"N. rows: {len(nba)}")
+print(nba.head(10))
 #print(nba.tail(10))
 
-print(nba.info)
+#print(nba.info)
+print(nba.loc[nba['fran_id'] == 'Lakers', 'team_id'].value_counts())
+print(nba.loc[nba['team_id'] == 'MNL', 'date_game'].max())
+nba['date_played'] = pd.to_datetime(nba['date_game'])
+print(nba.info())
+total_bos_points = nba.loc[nba['team_id'] == 'BOS', 'pts'].sum()
+years_bos_played = len(nba.loc[nba['team_id'] == 'BOS', 'date_played']\
+    .apply(lambda v: v.year).unique())
+avg_bos_points_per_year = total_bos_points / years_bos_played
+print(avg_bos_points_per_year)
+
