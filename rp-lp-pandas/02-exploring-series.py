cb1801a80c941e620d19aba342b9f5f085bfc697
Mauricio Mejia Castro <mauricio.mejia@promptlyhealth.com>
2022-11-14 00:17:42 +0000

    Add DataFrame creation examples

---
+++ rp-lp-pandas/02-exploring-series.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+revenues = pd.Series([5555, 7000, 1980])
+
+print(revenues.values)
+print(revenues.index)
+
+city_revenues = pd.Series(
+    [4200, 8000, 3453],
+    index=['Amsterdam', 'Toronto', 'Tokyo']
+)
+city_employee_count_data = {'Amsterdam': 5, 'Tokyo': 8}
+city_employee_count = pd.Series(city_employee_count_data)
+
+print(city_employee_count.keys())
+
+city_data = pd.DataFrame({
+    'revenue': city_revenues,
+    'employee_count': city_employee_count
+})
+
+print(city_data)
+
+# You can get the axes like this
+print(city_data.axes)
